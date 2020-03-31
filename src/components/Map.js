75baf4c9b0c3d9e0129b0663f891a4122f86553a
Jesus Ramirez <jesusdevelp@gmail.com>
2020-03-31 17:58:31 -0600

    Implementing map

---
+++ src/components/Map.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { GoogleMap, Marker, withGoogleMap } from "react-google-maps";
+
+const google = window.google;
+
+class Map extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    const MyMapComponent = withGoogleMap(props => (
+      <GoogleMap
+        defaultZoom={14}
+        position={{ lat: 19.39029, lng: -99.2828898 }}
+        defaultTitle="Mapa"
+      ></GoogleMap>
+    ));
+    return (
+      <MyMapComponent
+        loadingElement={<div style={{ height: "100%" }} />}
+        containerElement={<div style={{ height: "70vh" }} />}
+        mapElement={<div style={{ height: "100%" }} />}
+      />
+    );
+  }
+}
+
+export default Map;
